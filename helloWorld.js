4528e9bb1b02bc67963bff1337fb3c48bf78a265
sangitasurvase <144414092+sangitasurvase@users.noreply.github.com>
2023-12-26 10:10:57 +0530

    assignment
     first assignment completed

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,9 @@
 console.log('Hello js I am sangita');
+var num;
+console.log(num);
+var num=10;
+console.log(num);
+var myName='sangita survase';
+console.log(myName);
+console.log('first name',myName);
+console.log('full Name',myName);
